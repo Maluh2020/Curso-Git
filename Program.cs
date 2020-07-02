2f6f12ac28745f43f01a95d9427bbc580070097d
Malu Silva <maluhmyth@gmail.com>
2020-07-01 23:19:44 -0300

    Projeto iniciado com a mensagem iniciando no GitHub

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProjetoTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iniciando um repositório no GitHub");
+            Console.ReadLine();
+        }
+    }
+}
